78fec93921be4135101b19e1c2efd92a627efe6f
Lev <levbro14@gmail.com>
2026-07-02 20:36:01 +0700

    Update settings screen

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,29 +1,62 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _islight = true;
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
       children: [
-        ListTile(
-          leading: const Icon(Icons.notifications),
-          title: const Text('Уведомления'),
-          trailing: Switch(value: true, onChanged: (value) {}),
-        ),
-        ListTile(
-          leading: const Icon(Icons.palette),
-          title: const Text('Тема'),
-          trailing: const Icon(Icons.chevron_right),
-          onTap: () {},
-        ),
-        ListTile(
-          leading: const Icon(Icons.language),
-          title: const Text('Язык'),
-          trailing: const Icon(Icons.chevron_right),
-          onTap: () {},
-        ),
+        const SizedBox(height: 50),
+        Center(
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: SwitchListTile(
+                      title: Text("Сменить тему"),
+                      value: _islight, 
+                      onChanged: (value) {
+                        setState(() {
+                          _islight = value;
+                        });
+                      }
+                    ),
+                  ),
+                  const Divider(),
+                  SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: OutlinedButton(
+                      onPressed: (){}, 
+                      child: const Text("Очистить кэш")
+                    ),
+                  ),
+                  const Divider(),
+                  SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: OutlinedButton(
+                      onPressed: (){}, 
+                      child: const Text("data")
+                    ),
+                  ),    
+                ]
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
